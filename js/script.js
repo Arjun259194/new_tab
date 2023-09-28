dedc2ce949c8ed4fc83bc8b9655350d6df082f0d
arjun <arjun259194@gmail.com>
2023-09-28 18:05:49 +0530

    new structure

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
 //*ELEMENTS
 const cmdInput = document.querySelector("#cmd-input");
+const outputSection = document.querySelector("section#output-section")
 
 //*UTILS
-function appendToOutput(value, section) {
+function appendToOutput(output, cmd, section) {
+  if (cmd === "clear") return
+  if (cmd === "" || cmd === undefined) return
   const d = document.createElement("div")
   const p1 = document.createElement("p")
   const p2 = document.createElement("p")
-  p1.innerHTML = `$ ${value}`
-  p2.innerHTML = "command run successfully"
+  p1.innerHTML = `$ ${cmd}`
+  p2.innerHTML = output ?? "command not found"
   d.append(p1, p2)
   section.append(d)
 }
 
 //* HANDLER
 const cmdKeyHandler = (event) => {
-  const outputSection = document.querySelector("section#output-section")
   if (event.key === "Enter") {
     const inputValue = event.target.value;
-    appendToOutput(inputValue, outputSection)
+    cmdHandler(inputValue.trim(), cmdMap)
     event.target.value = ""
   }
-  console.log(event.target.value)
+}
+
+function cmdHandler(cmd, map) {
+  const fn = map.get(cmd)
+  if (!fn)
+    appendToOutput(undefined, cmd, outputSection)
+  const result = fn()
+  appendToOutput(result, cmd, outputSection)
 }
 
 //*EVENT-LISTENER
 cmdInput.addEventListener("keypress", cmdKeyHandler)
+
+//*CMD
+
+const cmdMap = new Map()
+
+cmdMap.set("clear", function () {
+  document.querySelector("section#output-section").innerHTML = ""
+  return ""
+})
+
+cmdMap.set("youtube", function () {
+  window.location.href = "https://www.youtube.com"
+  return "redirecting to youtube..."
+})
